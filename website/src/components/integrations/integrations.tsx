4e115d51a19ffcf7582cba55a6cdc1591b3d0f9b
jonny-wg2 <jonny@wgtwo.com>
2022-02-04 15:57:39 +0100

    remove logos

--- website/src/components/integrations/integrations.tsx
+++ website/src/components/integrations/integrations.tsx
@@ -20,46 +20,16 @@ function Component() {
             img={require("../../../static/img/vimla.png")}
           />
           <Image
-            alt="Altibox Logo"
-            className={styles.logo}
-            style={{ padding: "20px 0", height: "90px" }}
-            img={require("../../../static/img/altibox.png")}
-          />
-          <Image
             alt="Cisco"
             className={styles.logo}
             style={{ padding: "20px 0" }}
             img={require("../../../static/img/cisco.png")}
           />
           <Image
-            alt="3 Logo"
-            className={styles.logo}
-            style={{ padding: "24px 0" }}
-            img={require("../../../static/img/3.png")}
-          />
-          <Image
             alt="Hutch CKH Logo"
             className={styles.logo}
             style={{ padding: "20px 0" }}
             img={require("../../../static/img/ckh.png")}
-          />
-          <Image
-            alt="Atos Logo"
-            className={styles.logo}
-            style={{ padding: "20px 0", height: "80px" }}
-            img={require("../../../static/img/atos.png")}
-          />
-          <Image
-            alt="Televox Logo"
-            className={styles.logo}
-            style={{ padding: "20px 0" }}
-            img={require("../../../static/img/televox.png")}
-          />
-          <Image
-            alt="University of Maryland Logo"
-            className={styles.logo}
-            style={{ padding: "20px 0" }}
-            img={require("../../../static/img/university-of-maryland.png")}
           />
           <Image
             alt="e-Rate Logo"
@@ -80,29 +50,10 @@ function Component() {
             img={require("../../../static/img/enghouse-networks.png")}
           />
           <Image
-            alt="Happybytes Logo"
-            className={styles.logo}
-            style={{ padding: "24px 0" }}
-            img={require("../../../static/img/happybytes.png")}
-          />
-          <Image
             alt="Telenor Logo"
             className={styles.logo}
             style={{ padding: "24px 0" }}
             img={require("../../../static/img/telenor.png")}
-          />
-
-          <Image
-            alt="Fjordkraft Logo"
-            className={styles.logo}
-            style={{ padding: "24px 0" }}
-            img={require("../../../static/img/fjordkraft.png")}
-          />
-          <Image
-            alt="nortel Logo"
-            className={styles.logo}
-            style={{ padding: "24px 0", height: "80px" }}
-            img={require("../../../static/img/nortel.png")}
           />
           <Image
             alt="MKI Logo"
