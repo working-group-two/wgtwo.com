9b12f0173e3fc1eed3887d00922632ffe95423a7
Tibor Vukovic <tiborvukovic91@gmail.com>
2022-02-15 11:17:48 +0100

    Statically render jobs page + fetch XML from personio w/ GH action (#33)
    * Statically render jobs pages
    
    * Add GH action
    
    * Update website-sync-personio-jobs.yml
    
    * Update website-sync-personio-jobs.yml
    
    * Use curl instead of node for fetching personio xml
    
    U
    
    * Update website-sync-personio-jobs.yml
    
    * Fix GH action
    
    * Fix GH action
    
    * test
    
    * test
    
    * Sync personio jobs xml
    
    * Fix GH
    
    Co-authored-by: GitHub Action <your-username@users.noreply.github.com>

--- website/plugins/dynamic-routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (context, options) => ({
-  name: 'plugin-dynamic-routes',
-  contentLoaded: async ({ content, actions }) => {
-      const { routes } = options
-      const { addRoute } = actions
-      routes.map(route => addRoute(route))
-  }
-})
