ec35cbe288f398d4a36f6b5f8d14a4c4acf141ef
David <36410074+david-wg2@users.noreply.github.com>
2022-02-15 12:28:11 +0100

    Change linebreaks

--- website/src/components/hero/hero.tsx
+++ website/src/components/hero/hero.tsx
@@ -15,8 +15,9 @@ function Component() {
             {/* <span className={`${styles.ak} ${styles.hn} ${styles.ah} ${styles.av} ${styles.ab}`}>Mobile core network.</span> */}
           </h1>
           <div className={common.subtitle}>
-            No more boxes, no more end of life, no large upfront investments. With the <b>wgtwo</b> cloud native mobile core we offer continuous innovation, continuous development, scalability and flexibility. And to make it really simple for operators – delivered as a managed SaaS telecommunication network.
-            
+            No more boxes, no more end of life, no large upfront investments. With the <b>wgtwo</b> cloud native mobile 
+            core we offer continuous innovation, continuous development, scalability and flexibility. 
+            And to make it really simple for operators – delivered as a managed SaaS telecommunication network.
           </div>
           <div className={styles.buttons}>
             <Link to="/technology" className={`${common.button} ${common.buttonPrimary}`}>
