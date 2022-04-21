7dd12188e6905825e89f075517b8591f85d73bce
Steinar Utstrand <104059760+s-utstrand@users.noreply.github.com>
2022-04-21 15:33:11 +0200

    Newsletter signup form (#58)
    * Create newsletter signup component and add it to the home page
    
    * Add newsletter signup component to contact page
    
    * Replace "Privacy policy" with "Privacy notice"

---
+++ website/src/components/newsletter-signup/newsletter-signup.tsx
@@ -0,0 +1,61 @@
+import common from "../../css/common.module.css"
+import styles from "./newsletter-signup.module.css"
+import Link from "@docusaurus/Link"
+import Head from "@docusaurus/Head"
+import React from "react"
+
+const NewsletterSignup = () => {
+  return (
+    <div className={`${common.section} ${styles.newsletterSignup}`}>
+      <div className={common.container}>
+        <Head>
+          <script
+            type="text/javascript"
+            src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"
+          ></script>
+        </Head>
+
+        <div className={common.centeredText}>
+          <h2>Stay updated</h2>
+          <p>
+            Get the latest from Working Group Two. We'll send you news and
+            updates from time to time.{" "}
+          </p>
+
+          <form
+            className={`js-cm-form ${styles.formContainer}`}
+            id="subForm"
+            action="https://www.createsend.com/t/subscribeerror?description="
+            method="post"
+            data-id="191722FC90141D02184CB1B62AB3DC26955E76E0D27B7A1AB8E959FCE242FF63E7D5C217A62680441C6357AC1AAA7146FDBB1C4EB96CCAB4391278030E63DF48"
+          >
+            <input
+              type="email"
+              className="js-cm-email-input qa-input-email"
+              id="fieldEmail"
+              name="cm-jrduthd-jrduthd"
+              autoComplete="Email"
+              aria-label="Email"
+              placeholder="Email"
+              maxLength={200}
+              required
+            />
+            <button
+              className={`${common.button} ${common.buttonPrimary}`}
+              type="submit"
+            >
+              Subscribe
+            </button>
+          </form>
+          <p>
+            <Link to="/privacy" className={styles.privacyPolicyLink}>
+              Privacy notice
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewsletterSignup
