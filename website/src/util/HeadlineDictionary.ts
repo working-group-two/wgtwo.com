d90dc7be02e0d221d97eb2af549399d4e532c900
Prid (Lenovo) <prid_outing@hotmail.com>
2022-08-29 06:24:44 +0200

    TechCo Headline Generator Finished!

---
+++ website/src/util/HeadlineDictionary.ts
@@ -0,0 +1,98 @@
+/* DO NOT CHANGE ORDER OF ELEMENTS!!, add new ones at the end only.
+  Order is very important for shareable permalinks */
+
+export const actions = [
+  'launches',
+  'partners',
+  'acquires',
+  'discovers',
+  'introduces',
+  'joins forces with',
+  'raises','accelerates',
+  'integrates',
+  'wins',
+  'unveils',
+  'awards'
+];
+
+export const outcomes = [
+  'to achieve CAPEX reduction',
+  'to enable programmability',
+  'to achieve cost reductions',
+  'to become flexible',
+  'to gain innovation power',
+  'to speed up time-to-market',
+  'to radically simplify',
+  'to dominate the world 😈'
+];
+
+export const adjectives = [
+  'ultrafast',
+  'industry-leading',
+  'efficient',
+  'outstanding',
+  'refreshed',
+  'revolutionary',
+  'verified',
+  'standalone',
+  'modular',
+  'cloud-native',
+  'containerized',
+  'next-generation',
+  'off-premises',
+  'digitalized',
+  'serverless',
+  'Intelligent',
+  'dynamic',
+  'G-neutral',
+  'g-agnostic',
+  'future-proof'
+];
+
+export const nouns = [
+  'Evolved Packet Core',
+  '5G core',
+  '5G non-standalone',
+  'CDR',
+  'Edge Technologies',
+  'STP/DRA',
+  'vEPC',
+  'HSM',
+  'Gy/Ro',
+  'Packet Gateway',
+  'Private networks',
+  'Open RAN',
+  'GSM',
+  'Diameter',
+  'ATGW',
+  'HLR',
+  'Cloud-native Network Functions',
+  'CI/CD/CT ',
+  'HSP'
+  ,'Network Wide Provisioning',
+  'ePDG',
+  'PCF',
+  'Realtime Billing Gateway',
+  'API Gateway'
+  ,'Public DNS',
+  'S/PGW',
+  'HSS',
+  'GPRS Tunnelling Protocol',
+  'IMS',
+  'MMSC',
+  'Media Gateway',
+  'SIGTRAN',
+  'webRTC',
+  'SIP',
+  'SS7',
+  'IaaS',
+  'PaaS',
+  'MaaS',
+  'UCaaS'
+];
+
+export const endings = [
+  'delivered fully as-a-service',
+  'delivered with no end-of-life',
+  'delivered with a full product ecosystem'
+];
