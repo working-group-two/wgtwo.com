402497c53dc773c2f63de122f4278a34fda3ce9e
jonny-wg2 <jonny@wgtwo.com>
2022-10-13 21:34:34 +0200

    update to path

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           label: "Customers",
           position: "left",
           type: "dropdown",
-          to: "features",
+          to: "customers",
           items: [
             { label: "Vimla", href: "/" },
             { label: "CKH IOD", href: "/" },
