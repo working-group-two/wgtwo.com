1083f8d80d73d8a930671cf4437bb550af257174
Prid (Lenovo) <prid_outing@hotmail.com>
2022-10-23 05:57:10 +0200

    Blog Social Media Buttons
    Overrides blog Date section and Footer to include Sharing options.
    - New NPM module "react-menu" for fancy dropdown menu on click
    - Icons added
    - Separate file for social media window popup behavior
    - (unsafe) Blog sections overriden (might need to be tweaked on updating Docusaurus)

---
+++ website/src/theme/BlogPostItem/Footer/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useBlogPost} from '@docusaurus/theme-common/internal';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import EditThisPage from '@theme/EditThisPage';
+import TagsListInline from '@theme/TagsListInline';
+import ReadMoreLink from '@theme/BlogPostItem/Footer/ReadMoreLink';
+import styles from './styles.module.css';
+
+import { Share, Link } from 'lucide-react';
+import TwitterIcon from '@site/static/img/icons/twitter.svg'
+import LinkedinIcon from '@site/static/img/icons/linkedin.svg'
+import RedditIcon from '@site/static/img/icons/reddit.svg'
+
+import { openTwitterPopup, openRedditPopup, openLinkedinPopup } from "../../../util/social-share-behavior";
+
+import { Menu, MenuItem, MenuButton, MenuDivider } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
+
+// Social Media Button Handlers
+function onTwitterClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title + " " + url;
+  openTwitterPopup(text);
+}
+function onRedditClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title;
+  openRedditPopup(url, text);
+}
+function onLinkedinClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title;
+  openLinkedinPopup(url, text);
+}
+function onCopyLink(baseUrl, metadata) {
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  navigator.clipboard.writeText(url);
+}
+
+
+export default function BlogPostItemFooter() {
+  const {siteConfig} = useDocusaurusContext();
+  const baseUrl = siteConfig.url;
+
+  const {metadata, isBlogPostPage} = useBlogPost();
+  const {tags, title, editUrl, hasTruncateMarker} = metadata;
+  // A post is truncated if it's in the "list view" and it has a truncate marker
+  const truncatedPost = !isBlogPostPage && hasTruncateMarker;
+  const tagsExists = tags.length > 0;
+  const renderFooter = tagsExists || truncatedPost || editUrl;
+  if (!renderFooter) {
+    return null;
+  }
+  return (
+    <footer
+      className="row docusaurus-mt-lg">
+      {tagsExists && (
+        <div className="col--9">
+          <TagsListInline tags={tags} />
+        </div>
+      )}
+
+      {isBlogPostPage && (
+        <div className={clsx(`col ${styles.sharingSection}`, {'col--3': tagsExists})}>
+          <Menu menuButton={
+            <MenuButton className={styles.sharingButton}>
+              <Share size={20} className={styles.sharingIcon} />
+            </MenuButton>
+          }
+          className={styles.menuContainer}
+          align="center"
+          arrow
+          transition>
+            <MenuItem onClick={() => onCopyLink(baseUrl, metadata)}>
+              <Link size={20} className={styles.menuIcon} style={{fill: 'transparent'}} /> Copy link
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem onClick={() => onLinkedinClick(baseUrl, metadata)}>
+              <LinkedinIcon className={styles.menuIcon} /> Share on LinkedIn
+            </MenuItem>
+            <MenuItem onClick={() => onTwitterClick(baseUrl, metadata)}>
+              <TwitterIcon className={styles.menuIcon} /> Share on Twitter
+            </MenuItem>
+            <MenuItem onClick={() => onRedditClick(baseUrl, metadata)}>
+              <RedditIcon className={styles.menuIcon} /> Share on Reddit
+            </MenuItem>
+          </Menu>
+        </div>
+      )}
+
+      {isBlogPostPage && editUrl && (
+        <div className="col margin-top--sm">
+          <EditThisPage editUrl={editUrl} />
+        </div>
+      )}
+
+      {truncatedPost && (
+        <div
+          className={clsx('col text--right', {
+            'col--3': tagsExists,
+          })}>
+          <ReadMoreLink blogPostTitle={title} to={metadata.permalink} />
+        </div>
+      )}
+    </footer>
+  );
+}
+
+function removeTrailingSlash(string) {
+  return string.replace(/\/+$/, '');
+}
---
+++ website/src/theme/BlogPostItem/Header/Info/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import clsx from 'clsx';
+import {translate} from '@docusaurus/Translate';
+import {usePluralForm} from '@docusaurus/theme-common';
+import {useBlogPost} from '@docusaurus/theme-common/internal';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './styles.module.css';
+
+import TwitterIcon from '@site/static/img/icons/twitter.svg'
+import LinkedinIcon from '@site/static/img/icons/linkedin.svg'
+import RedditIcon from '@site/static/img/icons/reddit.svg'
+import { Link } from 'lucide-react'
+
+import { openTwitterPopup, openRedditPopup, openLinkedinPopup } from "../../../../util/social-share-behavior";
+
+// Social Media Button Handlers
+function onTwitterClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title + " " + url;
+  openTwitterPopup(text);
+}
+function onRedditClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title;
+  openRedditPopup(url, text);
+}
+function onLinkedinClick(baseUrl, metadata) {
+  const title = metadata.title;
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  const text = title;
+  openLinkedinPopup(url, text);
+}
+function onCopyLink(baseUrl, metadata) {
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
+  navigator.clipboard.writeText(url);
+}
+
+
+// Very simple pluralization: probably good enough for now
+function useReadingTimePlural() {
+  const {selectMessage} = usePluralForm();
+  return (readingTimeFloat) => {
+    const readingTime = Math.ceil(readingTimeFloat);
+    return selectMessage(
+      readingTime,
+      translate(
+        {
+          id: 'theme.blog.post.readingTime.plurals',
+          description:
+            'Pluralized label for "{readingTime} min read". Use as much plural forms (separated by "|") as your language support (see https://www.unicode.org/cldr/cldr-aux/charts/34/supplemental/language_plural_rules.html)',
+          message: 'One min read|{readingTime} min read',
+        },
+        {readingTime},
+      ),
+    );
+  };
+}
+function ReadingTime({readingTime}) {
+  const readingTimePlural = useReadingTimePlural();
+  return <>{readingTimePlural(readingTime)}</>;
+}
+function Date({date, formattedDate}) {
+  return (
+    <time dateTime={date} itemProp="datePublished">
+      {formattedDate}
+    </time>
+  );
+}
+function Spacer() {
+  return <>{' · '}</>;
+}
+export default function BlogPostItemHeaderInfo({className}) {
+  const {siteConfig} = useDocusaurusContext();
+  const baseUrl = siteConfig.url;
+
+  const {metadata} = useBlogPost();
+  const {date, formattedDate, readingTime} = metadata;
+  return (
+    <div className={clsx(styles.container, 'margin-vert--md', className)}>
+      <div className={styles.dateContainer}>
+        <Date date={date} formattedDate={formattedDate} />
+        {typeof readingTime !== 'undefined' && (
+          <>
+            <Spacer />
+            <ReadingTime readingTime={readingTime} />
+          </>
+        )}
+      </div>
+      <div className={styles.iconsContainer}>
+        <button className={styles.button} onClick={() => onLinkedinClick(baseUrl, metadata)}>
+          <LinkedinIcon className={styles.icon} />
+        </button>
+        <button className={styles.button} onClick={() => onTwitterClick(baseUrl, metadata)}>
+          <TwitterIcon className={styles.icon} />
+        </button>
+        <button className={styles.button} onClick={() => onRedditClick(baseUrl, metadata)}>
+          <RedditIcon className={styles.icon} />
+        </button>
+        <span className={styles.spacer}>|</span>
+        <button className={styles.button} onClick={() => onCopyLink(baseUrl, metadata)}>
+          <Link className={`${styles.icon} ${styles.iconLink}`} strokeWidth={2.5} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function removeTrailingSlash(string) {
+  return string.replace(/\/+$/, '');
+}
