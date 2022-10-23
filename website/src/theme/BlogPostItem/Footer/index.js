286bcccafe3ee7ddf70c4cce4fe5a9164f9ffa67
Prid (Lenovo) <prid_outing@hotmail.com>
2022-10-23 06:06:23 +0200

    Prettier

--- website/src/theme/BlogPostItem/Footer/index.js
+++ website/src/theme/BlogPostItem/Footer/index.js
@@ -1,64 +1,66 @@
-import React from 'react';
-import clsx from 'clsx';
-import {useBlogPost} from '@docusaurus/theme-common/internal';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import EditThisPage from '@theme/EditThisPage';
-import TagsListInline from '@theme/TagsListInline';
-import ReadMoreLink from '@theme/BlogPostItem/Footer/ReadMoreLink';
-import styles from './styles.module.css';
+import React from "react"
+import clsx from "clsx"
+import { useBlogPost } from "@docusaurus/theme-common/internal"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import EditThisPage from "@theme/EditThisPage"
+import TagsListInline from "@theme/TagsListInline"
+import ReadMoreLink from "@theme/BlogPostItem/Footer/ReadMoreLink"
+import styles from "./styles.module.css"
 
-import { Share, Link } from 'lucide-react';
-import TwitterIcon from '@site/static/img/icons/twitter.svg'
-import LinkedinIcon from '@site/static/img/icons/linkedin.svg'
-import RedditIcon from '@site/static/img/icons/reddit.svg'
+import { Share, Link } from "lucide-react"
+import TwitterIcon from "@site/static/img/icons/twitter.svg"
+import LinkedinIcon from "@site/static/img/icons/linkedin.svg"
+import RedditIcon from "@site/static/img/icons/reddit.svg"
 
-import { openTwitterPopup, openRedditPopup, openLinkedinPopup } from "../../../util/social-share-behavior";
+import {
+  openTwitterPopup,
+  openRedditPopup,
+  openLinkedinPopup,
+} from "../../../util/social-share-behavior"
 
-import { Menu, MenuItem, MenuButton, MenuDivider } from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
-import '@szhsin/react-menu/dist/transitions/slide.css';
+import { Menu, MenuItem, MenuButton, MenuDivider } from "@szhsin/react-menu"
+import "@szhsin/react-menu/dist/index.css"
+import "@szhsin/react-menu/dist/transitions/slide.css"
 
 // Social Media Button Handlers
 function onTwitterClick(baseUrl, metadata) {
-  const title = metadata.title;
-  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
-  const text = title + " " + url;
-  openTwitterPopup(text);
+  const title = metadata.title
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink
+  const text = title + " " + url
+  openTwitterPopup(text)
 }
 function onRedditClick(baseUrl, metadata) {
-  const title = metadata.title;
-  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
-  const text = title;
-  openRedditPopup(url, text);
+  const title = metadata.title
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink
+  const text = title
+  openRedditPopup(url, text)
 }
 function onLinkedinClick(baseUrl, metadata) {
-  const title = metadata.title;
-  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
-  const text = title;
-  openLinkedinPopup(url, text);
+  const title = metadata.title
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink
+  const text = title
+  openLinkedinPopup(url, text)
 }
 function onCopyLink(baseUrl, metadata) {
-  const url = removeTrailingSlash(baseUrl) + metadata.permalink;
-  navigator.clipboard.writeText(url);
+  const url = removeTrailingSlash(baseUrl) + metadata.permalink
+  navigator.clipboard.writeText(url)
 }
 
-
 export default function BlogPostItemFooter() {
-  const {siteConfig} = useDocusaurusContext();
-  const baseUrl = siteConfig.url;
+  const { siteConfig } = useDocusaurusContext()
+  const baseUrl = siteConfig.url
 
-  const {metadata, isBlogPostPage} = useBlogPost();
-  const {tags, title, editUrl, hasTruncateMarker} = metadata;
+  const { metadata, isBlogPostPage } = useBlogPost()
+  const { tags, title, editUrl, hasTruncateMarker } = metadata
   // A post is truncated if it's in the "list view" and it has a truncate marker
-  const truncatedPost = !isBlogPostPage && hasTruncateMarker;
-  const tagsExists = tags.length > 0;
-  const renderFooter = tagsExists || truncatedPost || editUrl;
+  const truncatedPost = !isBlogPostPage && hasTruncateMarker
+  const tagsExists = tags.length > 0
+  const renderFooter = tagsExists || truncatedPost || editUrl
   if (!renderFooter) {
-    return null;
+    return null
   }
   return (
-    <footer
-      className="row docusaurus-mt-lg">
+    <footer className="row docusaurus-mt-lg">
       {tagsExists && (
         <div className="col--9">
           <TagsListInline tags={tags} />
@@ -66,18 +68,29 @@ export default function BlogPostItemFooter() {
       )}
 
       {isBlogPostPage && (
-        <div className={clsx(`col ${styles.sharingSection}`, {'col--3': tagsExists})}>
-          <Menu menuButton={
-            <MenuButton className={styles.sharingButton}>
-              <Share size={20} className={styles.sharingIcon} />
-            </MenuButton>
-          }
-          className={styles.menuContainer}
-          align="center"
-          arrow
-          transition>
+        <div
+          className={clsx(`col ${styles.sharingSection}`, {
+            "col--3": tagsExists,
+          })}
+        >
+          <Menu
+            menuButton={
+              <MenuButton className={styles.sharingButton}>
+                <Share size={20} className={styles.sharingIcon} />
+              </MenuButton>
+            }
+            className={styles.menuContainer}
+            align="center"
+            arrow
+            transition
+          >
             <MenuItem onClick={() => onCopyLink(baseUrl, metadata)}>
-              <Link size={20} className={styles.menuIcon} style={{fill: 'transparent'}} /> Copy link
+              <Link
+                size={20}
+                className={styles.menuIcon}
+                style={{ fill: "transparent" }}
+              />{" "}
+              Copy link
             </MenuItem>
             <MenuDivider />
             <MenuItem onClick={() => onLinkedinClick(baseUrl, metadata)}>
@@ -101,16 +114,17 @@ export default function BlogPostItemFooter() {
 
       {truncatedPost && (
         <div
-          className={clsx('col text--right', {
-            'col--3': tagsExists,
-          })}>
+          className={clsx("col text--right", {
+            "col--3": tagsExists,
+          })}
+        >
           <ReadMoreLink blogPostTitle={title} to={metadata.permalink} />
         </div>
       )}
     </footer>
-  );
+  )
 }
 
 function removeTrailingSlash(string) {
-  return string.replace(/\/+$/, '');
+  return string.replace(/\/+$/, "")
 }
--- website/src/util/social-share-behavior.ts
+++ website/src/util/social-share-behavior.ts
@@ -1,14 +1,26 @@
 export function openTwitterPopup(text) {
-  const url = "http://twitter.com/intent/tweet?text=";
-  window.open(url + text, 'twitterwindow','left=20,top=20,width=600,height=300,toolbar=0,resizable=1');
+  const url = "http://twitter.com/intent/tweet?text="
+  window.open(
+    url + text,
+    "twitterwindow",
+    "left=20,top=20,width=600,height=300,toolbar=0,resizable=1"
+  )
 }
 
 export function openRedditPopup(url, text) {
-  const shareUrl = `http://www.reddit.com/submit?url=${url}&title=${text}`;
-  window.open(shareUrl, 'redditwindow','left=20,top=20,width=600,height=500,toolbar=0,resizable=1');
+  const shareUrl = `http://www.reddit.com/submit?url=${url}&title=${text}`
+  window.open(
+    shareUrl,
+    "redditwindow",
+    "left=20,top=20,width=600,height=500,toolbar=0,resizable=1"
+  )
 }
 
 export function openLinkedinPopup(url, text) {
-  const shareUrl = `http://www.linkedin.com/shareArticle?url=${url}&title=${text}`;
-  window.open(shareUrl, 'linkedinwindow','left=20,top=20,width=600,height=500,toolbar=0,resizable=1');
+  const shareUrl = `http://www.linkedin.com/shareArticle?url=${url}&title=${text}`
+  window.open(
+    shareUrl,
+    "linkedinwindow",
+    "left=20,top=20,width=600,height=500,toolbar=0,resizable=1"
+  )
 }
