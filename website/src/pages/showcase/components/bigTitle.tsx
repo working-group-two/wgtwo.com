bf2195ab30bc698f66bed9d069e57c78267222f7
Prid (Lenovo) <prid_outing@hotmail.com>
2022-10-27 17:39:20 +0200

    Prettier

--- website/src/pages/showcase/components/bigTitle.tsx
+++ website/src/pages/showcase/components/bigTitle.tsx
@@ -3,19 +3,16 @@ import common from "../../../css/common.module.css"
 import styles from "./bigTitle.module.css"
 
 function Component({ pillText, titleText, logoUrl, children }) {
-  let logo;
+  let logo
 
-  if( logoUrl )
-    logo = <img className={styles.logo} src={logoUrl} />;
-  
+  if (logoUrl) logo = <img className={styles.logo} src={logoUrl} />
+
   return (
     <div>
       <div className={common.pillTitle}>{pillText}</div>
       <div className={styles.header}>
         <h2 className={common.title}>{titleText}</h2>
-        <div className={styles.logoContainer}>
-          {logo}
-        </div>
+        <div className={styles.logoContainer}>{logo}</div>
       </div>
       <div className={common.subtitle}>{children}</div>
     </div>
--- website/src/pages/showcase/components/section.tsx
+++ website/src/pages/showcase/components/section.tsx
@@ -4,8 +4,13 @@ import styles from "./section.module.css"
 
 function Component({ bgColor, children }) {
   return (
-    <div className={`${common.section} ${styles.sectionStyle}`} style={{ backgroundColor: bgColor }}>
-      <div className={`${common.container} ${styles.containerStyle}`}>{children}</div>
+    <div
+      className={`${common.section} ${styles.sectionStyle}`}
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className={`${common.container} ${styles.containerStyle}`}>
+        {children}
+      </div>
     </div>
   )
 }
