94a3838f994834892abfab61e93f6ae99b2227b4
Prid13 <Prid13@users.noreply.github.com>
2022-10-27 12:35:31 +0000

    Deploying to dev-pages from @ working-group-two/wgtwo.com@02f0c84607b96591e08a25a18daf42772fde1d78 🚀

---
+++ assets/js/92999a1c.d01f1e14.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkworking_group_two_website=self.webpackChunkworking_group_two_website||[]).push([[8442],{15310:function(e){e.exports=JSON.parse('{"permalink":"/blog/page/3","page":3,"postsPerPage":10,"totalPages":4,"totalCount":32,"previousPage":"/blog/page/2","nextPage":"/blog/page/4","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
