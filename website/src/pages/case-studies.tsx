bf66467e3ece38d9f01f8571e4f6a1311525f390
Prid (Lenovo) <prid_outing@hotmail.com>
2022-11-22 19:33:50 +0100

    Update case-studies.tsx

--- website/src/pages/case-studies.tsx
+++ website/src/pages/case-studies.tsx
@@ -73,6 +73,7 @@ function CaseStudies() {
 
 function sendMessage(){
   message(
+    
     `Message from wgtwo.com/case-studies\nName: ${form.name.current.value}\nEmail: ${form.email.current.value}\nMessage: ${form.message.current.value}`
   )
 
