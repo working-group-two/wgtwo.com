1abb6393140996efba83efa590e5fe3b955d8e87
torotimes <98818609+torotimes@users.noreply.github.com>
2023-01-20 14:39:56 +0000

    Deploying to gh-pages from @ working-group-two/wgtwo.com@efc903ae9b45d4afe7f040a00cba7553c7802bb9 🚀

--- jp/assets/js/057fb1fe.627c8a74.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkworking_group_two_website=self.webpackChunkworking_group_two_website||[]).push([[6254],{71467:function(e){e.exports=JSON.parse('{"permalink":"/jp/blog/tags/press-release","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
