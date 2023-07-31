d44a0c0e699093d16c74c5daea22df6c214395e6
Prid13 <prid_outing@hotmail.com>
2023-08-01 01:09:50 +0200

    Create sendmail.js

---
+++ netlify/functions/sendmail/sendmail.js
@@ -0,0 +1,27 @@
+const client = require("@sendgrid/mail")
+const { SENDGRID_APIKEY } = process.env
+
+exports.handler = async function (event, context, callback) {
+  const { message, senderEmail, senderName } = event.queryStringParameters
+  client.setApiKey(SENDGRID_APIKEY)
+
+  const data = {
+    to: "lalomaf902@mliok.com",
+    from: "noreply@wgtwo.com",
+    subject: `New message from ${senderName} (${senderEmail})`,
+    html: message,
+  }
+
+  try {
+    await client.send(data)
+    return {
+      statusCode: 200,
+      body: "Message sent",
+    }
+  } catch (err) {
+    return {
+      statusCode: err.code,
+      body: JSON.stringify({ msg: err.message }),
+    }
+  }
+}
