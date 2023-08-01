59db14ba74182106d9c9972acf349efc3f66c7f4
Prid13 <prid_outing@hotmail.com>
2023-08-01 02:21:29 +0200

    Update sendmail.js

--- netlify/functions/sendmail/sendmail.js
+++ netlify/functions/sendmail/sendmail.js
@@ -3,7 +3,7 @@ const { SENDGRID_APIKEY } = process.env
 
 exports.handler = async function (event, context, callback) {
   const { message, senderEmail, senderName } = event.queryStringParameters
-  client.setApiKey(SENDGRID_APIKEY)
+  //client.setApiKey(SENDGRID_APIKEY)
 
   const data = {
     to: "lalomaf902@mliok.com",
@@ -13,10 +13,10 @@ exports.handler = async function (event, context, callback) {
   }
 
   try {
-    await client.send(data)
+    //await client.send(data)
     return {
       statusCode: 200,
-      body: "Message sent",
+      body: JSON.stringify({ msg: err.message }),
     }
   } catch (err) {
     return {
