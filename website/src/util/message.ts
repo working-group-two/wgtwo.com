b8d06a551e915a171762e95d3dcd469d003ec756
Prid (Lenovo) <prid_outing@hotmail.com>
2023-08-19 03:53:18 +0200

    Add Netlify Sendgrid Function call

--- website/src/util/message.ts
+++ website/src/util/message.ts
@@ -1,5 +1,6 @@
-export default function sendMessage(message: string) {
-  return fetch(
+export default function sendMessage(message: string, fromName: string, fromEmail: string) {
+  /* Send Slack notification: */
+  fetch(
     atob(
       "aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDdCOTlMQkRNL0IwMzEwRjYxVUNDLzNsU0xVQVJmWk52YWlubXpKT0VUUEVGdAo="
     ),
@@ -7,5 +8,11 @@ export default function sendMessage(message: string) {
       method: "POST",
       body: `{"text":"${message.replace(/"/g, '\\"')}"}`,
     }
+  )
+
+  /* Send confirmation mail to user (via Netlify and SendGrid): */
+  const url = "aHR0cHM6Ly93Z3R3by1kZXYubmV0bGlmeS5hcHAvLm5ldGxpZnkvZnVuY3Rpb25zL3NlbmRtYWlsPw==";
+  fetch(
+    `${atob(url)}senderEmail=${fromEmail}&senderName=${fromName}&message=${message}`
   )
 }
